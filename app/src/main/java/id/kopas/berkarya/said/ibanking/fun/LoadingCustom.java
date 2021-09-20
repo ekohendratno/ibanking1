8c99095881104a7471d6aa7bee21ec4eaf813e04
Eko Hendratno <eko.hendratno@gmail.com>
2021-09-20 15:24:23 +0700

    up3

---
+++ app/src/main/java/id/kopas/berkarya/said/ibanking/fun/LoadingCustom.java
@@ -0,0 +1,32 @@
+package id.kopas.berkarya.said.ibanking.fun;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+
+import id.kopas.berkarya.said.ibanking.R;
+
+public class LoadingCustom extends Dialog {
+
+    public Activity c;
+    public LoadingCustom(Activity a) {
+        super(a);
+        // TODO Auto-generated constructor stub
+        this.c = a;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.loading);
+
+    }
+
+}
